cc1c919832549bcf7fe27c2da450a77e2d5daa40
Xagar123 <sagarcoc2000@gmail.com>
2022-08-12 12:35:47 +0530

    UC1 added

--- Employee.java
+++ Employee.java
@@ -1,6 +1,14 @@
+import javax.swing.*;
 import java.util.*;
 public class Employee {
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage progrsm");
+        int present = 1;
+        double employe_check = Math.floor(Math.random() * 10) % 2;
+        if (employe_check == present) {
+            System.out.println("Employe is present" );
+        } else {
+            System.out.println("Employee is absent" );
+        }
     }
 }
